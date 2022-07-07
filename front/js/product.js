1c115ae068aa5c854778b071c57e4f53bd0cbd8f
Thomas FAISCA <faisca.thomas@gmail.com>
2022-07-07 10:43:35 +0200

    Première version complète page produit

--- front/js/product.js
+++ front/js/product.js
@@ -59,9 +59,10 @@ addButton.addEventListener('click', (event) => {
             quantity: quantity.value
         };
         console.log(productChoice);
-        addToCart(productChoice); //Appel de la fonction pour l'ajout du produit
+        addToCart(productChoice); //Appel de la fonction pour l'ajout du produit au panier
 
         //-------------------------------Enregistrement produit dans le localStorage-------------------------------
+
         //Fonction pour la récupération du panier dans le localStorage
         function getCart() {
             let cart = localStorage.getItem("cart");
@@ -73,16 +74,23 @@ addButton.addEventListener('click', (event) => {
             }
         }
 
-            //Fonction ajout article supplémentaire au panier
+            //Fonction ajout article supplémentaire au panier en vérifiant si le même produit avec le même colori existe déjà
     
         function addToCart(productChoice) {
             let cart = getCart();
-            cart.push(productChoice);
+            let foundSameProduct = cart.find(productChoiceId => productChoiceId.id == productChoice.id) && cart.find(productChoiceColor => productChoiceColor.color == productChoice.color);
+            if(foundSameProduct != undefined) {
+                foundSameProduct.quantity = parseInt(foundSameProduct.quantity) + parseInt(productChoice.quantity);
+            }
+            else {
+                productChoice.quantity = quantity.value; 
+                cart.push(productChoice);
+            }
             saveCart(cart);
             alert("Produit ajouté au panier");
         }
 
-            //Fonction sauvegarde du panier
+            //Fonction sauvegarde du panier dans le localStorage
 
         function saveCart(cart) {
             localStorage.setItem("cart", JSON.stringify(cart));
